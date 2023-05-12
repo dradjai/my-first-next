2907af21f9a144ad42bbf54c7be222160895e215
David Radjai <dradjai@yahoo.com>
2023-05-12 11:42:38 -0400

    routing, fetching, layouts in nextjs

---
+++ app/whatev/Hero.jsx
@@ -0,0 +1,6 @@
+export default function Hero(){
+
+  return(
+    <h2 className="p-0 text-9xl font-semibold text-amber-600">Hero</h2>
+  )
+}
---
+++ app/whatev/page.jsx
@@ -0,0 +1,17 @@
+import Hero from "./Hero";
+
+
+
+export default function Page() {
+
+  
+
+  return(
+    <main className="p-8">
+      <Hero />
+      <h1 className="font-bold text-2xl">Whatev</h1>
+      <p>Routes in Next.js are too easy</p>
+      
+    </main>
+  )
+}
